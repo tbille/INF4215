e3ccd389dc9e1f339256a32b42b592166386b39f
Jm-ROBIN <JeanMarie@Sansfil-Securise-Etudiants-Lassonde-243-51.polymtl.ca>
2015-02-03 10:23:56 -0500

    Ajout Tile, implt methods

--- TP1/tangram.py
+++ TP1/tangram.py
@@ -2,7 +2,38 @@ from node import *
 from state import *
 from astar_search import *
 
-class Tangram:
+class Tangram(State):
     def __init__(self,_pattern,_pieces):
         self.pattern = _pattern
         self.pieces = _pieces
+
+    # Checks whether current state and the one passed as parameter are exactly the same
+    def equals(self,state):
+        return False
+
+    # Checks whether the state is a goal state
+    def isGoal(self):
+        return False
+
+    # Prints to the console a description of the state
+    def show(self):
+        pass
+
+    # State is updated according to action
+    def executeAction(self,action):
+        pass
+
+    # Returns a list of possible actions with the current state
+    def possibleAction(self):
+        return []
+
+    # Returns the cost of executing some action
+    # By default, we suppose that all actions have the same cost = 1
+    def cost(self,action):
+        return 1
+
+    # Returns a heuristic value that provides an estimate of the remaining
+    # cost to achieve the goal
+    # By default, value is 0
+    def heuristic(self):
+        return 0
