2753144d92d74c884f5d67ed8cb2a7e7cb7d3fa1
tbille <thomas.bille625@gmail.com>
2015-03-18 14:31:29 -0400

    thomas

--- TP2/personnes.pl
+++ TP2/personnes.pl
@@ -1,11 +1,14 @@
-chercher(_,[],_):-fail.
-chercher(Nom,[Nom|_],LAttributs):-verifieAttributs(Nom,LAttributs),!.
-chercher(Nom,[_|Q],LAttributs):-chercher(Nom,Q,LAttributs).
+chercher([],_,[]).
+chercher( [Nom|Q],LAttributs,[Nom|Res] ):-
+  verifieAttributs(Nom,LAttributs),!,
+  chercher(Q,LAttributs,Res).
+
+chercher([_|Q],LAttributs,LNoms):-chercher(Q,LAttributs,LNoms).
 
 
 verifieAttributs(_,[]).
 verifieAttributs(Nom,[Attribut|Q]):-
-  personne(Nom,_,LAttributs),
+  personne(Nom,LAttributs),
   member(Attribut,LAttributs),
   verifieAttributs(Nom,Q).
 
@@ -15,40 +18,25 @@ verifieAttributs(Nom,[Attribut|Q]):-
 %
 %-------------------------------------------
 
-% musiciens
 personne(michael_Jackson,[musicien,masculin,pop,mort]).
 personne(wolfgang_Amadeus_Mozart,[musicien,masculin,classique,mort]).
 personne(john_Lewis,[musicien,masculin,jazz,mort]).
-
-% politiciens
 personne(stephen_Harper,[politicien,masculin,vivant,canada]).
 personne(cleopatre_VII,[politicien,feminin,mort,egypte]).
-
-% acteurs
 personne(brad_Pitt,[acteur,masculin,vivant]).
 personne(julie_Snyder,[acteur,feminin,vivant]).
-
-% professeur
 personne(michel_Gagnon,[professeur,masculin,vivant,ia]).
 personne(michel_Dagenais,[professeur,masculin,vivant,logiciel_Libre]).
-
-% fictif
 personne(lara_croft,[fictif,feminin,jeuVideo]).
 personne(mario,[fictif,masculin,jeuVideo]).
 personne(garfield,[fictif,chat,masculin]).
 personne(james_Bond,[fictif,masculin,film]).
 personne(blanche_neige,[fictif,feminin,film]).
-
-% sportif
 personne(jacques_Villeneuve,[sportif,masculin,f1]).
 personne(rafael_Nadal,[sportif,masculin,tennis]).
 personne(eugenie_Bouchard,[sportif,feminin,tennis]).
-
-% auteur
 personne(victor_Hugo,[auteur,masculin,mort]).
 personne(j_K_Rowling,[auteur,feminin,vivant]).
-
-% religieux
 personne(pape_Francois,[religieux,masculin,vivant]).
 personne(moise,[religieux,masculin,marcher,mort]).
 personne(jesus,[religieux,masculin,separer,mort]).
