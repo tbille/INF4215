4bdfed713e589cb4ec99f191c4373fa7f42c9c8b
Jean-Marie Robin <JeanMarie@Sansfil-Securise-Etudiants-Lassonde-247-147.polymtl.ca>
2015-03-18 14:45:38 -0400

    prop part1 correc

--- TP2/personnes.pl
+++ TP2/personnes.pl
@@ -19,49 +19,27 @@ verifieAttributs(Nom,[Attribut|Q]):-
 %-------------------------------------------
 
 personne(michael_Jackson).
-	[musicien,masculin,pop,mort]).
 personne(wolfgang_Amadeus_Mozart).
-	[musicien,masculin,classique,mort]).
 personne(john_Lewis).
-	[musicien,masculin,jazz,mort]).
 personne(stephen_Harper).
-	[politicien,masculin,vivant,canada]).
 personne(cleopatre_VII).
-	[politicien,feminin,mort,egypte]).
 personne(brad_Pitt).
-	[acteur,masculin,vivant]).
 personne(julie_Snyder).
-	[acteur,feminin,vivant]).
 personne(michel_Gagnon).
-	[professeur,masculin,vivant,ia]).
 personne(michel_Dagenais).
-	[professeur,masculin,vivant,logiciel_Libre]).
 personne(lara_croft).
-	[fictif,feminin,jeuVideo]).
 personne(mario).
-	[fictif,masculin,jeuVideo]).
 personne(garfield).
-	[fictif,chat,masculin]).
 personne(james_Bond).
-	[fictif,masculin,film]).
 personne(blanche_neige).
-	[fictif,feminin,film]).
 personne(jacques_Villeneuve).
-	[sportif,masculin,f1]).
 personne(rafael_Nadal).
-	[sportif,masculin,tennis]).
 personne(eugenie_Bouchard).
-	[sportif,feminin,tennis]).
 personne(victor_Hugo).
-	[auteur,masculin,mort]).
 personne(j_K_Rowling).
-	[auteur,feminin,vivant]).
 personne(pape_Francois).
-	[religieux,masculin,vivant]).
 personne(moise).
-	[religieux,masculin,marcher,mort]).
 personne(jesus).
-	[religieux,masculin,separer,mort]).
 
 %-------------------------------------------
 %
