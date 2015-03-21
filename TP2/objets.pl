341bdea5d04609a37cc64a78cf096d27c81ba778
Jean-Marie Robin <JeanMarie@MacBook-Pro-de-Jean-Marie.local>
2015-03-21 16:49:13 -0400

    prop part 1

--- TP2/objets.pl
+++ TP2/objets.pl
@@ -1,4 +1,10 @@
 
+%-------------------------------------------
+%
+%   Recherche Objets
+%
+%-------------------------------------------
+
 ask(materiel,Materiel):-
   format("Votre objet est-il compose principalement de ~w ? ",[Materiel]),
   read(Reponse),
@@ -14,6 +20,12 @@ ask(fonction,Fonction):-
   read(Reponse),
   Reponse = oui.
 
+
+%-------------------------------------------
+%
+%   Objets
+%
+%-------------------------------------------
 
 objets(aspirateur).
 objets(assiette).
@@ -56,12 +68,84 @@ objets(lunettes).
 objets(enveloppe).
 
 
+%-------------------------------------------
+%
+%   Props
+%
+%-------------------------------------------
 
 
+prop(aspirateur,electromenager).
+prop(aspirateur,entretien).
+prop(aspirateur,plastique).
+prop(aspirateur,fixe).
+prop(aspirateur,).   %----------------------
 
+prop(assiette,portable).
+prop(assiette,cuisine).
+prop(assiette,vaisselle).
 
+prop(balai,cuisine).
+prop(balai,entretien).
 
+prop(cactus,decoration).
 
+prop(cafetiere,electromenager).
+prop(cafetiere,cuisine).
+prop(cafetiere,plastique).
+
+prop(casserole,cuisine).
+prop(casserole,vaisselle).
+prop(casserole,contenur).
+prop(casserole,metal).
+
+prop(cle,portable).
+prop(cle,poche).
+prop(cle,metal).
+
+prop(cuisiniere,electromenager).
+prop(cuisiniere,cuisine).
+prop(cuisiniere,metal).
+
+prop(detergent_a_vaisselle,cuisine).
+prop(detergent_a_vaisselle,entretien).
+prop(detergent_a_vaisselle,laver).
+
+prop(four_micro-onde,electromenager).
+prop(four_micro-onde,cuisine).
+prop(four_micro-onde,contenant).
+prop(four_micro-onde,metal).
+
+prop(fourchette,cuisine).
+prop(fourchette,vaisselle).
+prop(fourchette,metal).
+
+prop(grille-pain,electromenager).
+prop(grille-pain,cuisine).
+prop(grille-pain,contenant).
+prop(grille-pain,metal).
+
+prop(lampe,chambre).
+prop(lampe,decoration).
+prop(lampe,metal).
+
+prop(lit,bois).
+prop(lit,chambre).
+
+prop(ordinateur,electronique).
+prop(ordinateur,metal).
+prop(ordinateur,bureau).
+
+prop(papier,chambre).
+prop(papier,papier).
+
+prop(piano,decoration).
+prop(piano,musique).
+prop(piano,bois).
+
+prop(porte-feuille,portable).
+prop(porte-feuille,poche).
+prop(porte-feuille,cuir).
 
 
 prop(enveloppe,).
@@ -69,7 +153,11 @@ prop(enveloppe,).
 prop(enveloppe,).
 prop(enveloppe,).
 
-
+%-------------------------------------------
+%
+%   Attributs
+%
+%-------------------------------------------
 
 localisation(cuisine).
 localisation(chambre).
