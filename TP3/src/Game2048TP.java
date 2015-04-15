febaeca144b196260765d2194cf22fbfd508f59e
tbille <thomas.bille625@gmail.com>
2015-04-15 16:13:40 -0400

    Ajoute case

--- TP3/src/Game2048TP.java
+++ TP3/src/Game2048TP.java
@@ -10,17 +10,26 @@ public class Game2048TP {
 		PlayingField pf = new PlayingField(4,4);
 		pf.initialize(2);
 		System.out.println(pf);
-		
+		double res;
+		String str;
 		while(pf.movesAvailable()){
-			System.out.print("Enter direction (u,d,l,r): ");
-	        // Read the char
-			Scanner s = new Scanner(System.in);
-			String str = s.nextLine();
-			
+
+			res=Math.random()*100;
+			System.out.println(res);
+			if(res<25)
+				str="u";
+			else if(res<50)
+				str="l";
+			else if(res<75)
+				str="d";
+			else
+				str="r";
+			System.out.println(str);
 			int moved = pf.moveByString(str);
 	        if(moved>0) pf.insertRandCell();
 	        System.out.println(pf);
 		}
+		System.out.println(pf);
 	}
 	
 }
