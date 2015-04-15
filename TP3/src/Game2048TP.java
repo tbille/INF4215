b10fb2bb553b6bc8dd2c6caa68285b894c076b95
tbille <thomas.bille625@gmail.com>
2015-04-15 17:41:14 -0400

    GOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- TP3/src/Game2048TP.java
+++ TP3/src/Game2048TP.java
@@ -8,54 +8,134 @@ import java.util.Scanner;
 public class Game2048TP {
 
 	public static void main(String[] args) throws IOException {
-		PlayingField pf = new PlayingField(4,4);
-		pf.initialize(2);
-		System.out.println(pf);
+
 		double res;
 		String str;
 		Analyser firstAnalyser;
-		int beforeMove=0;
+		int previousQuality;
+		String previousMove;
 		
-		
-		
-		while(pf.movesAvailable()){
-			firstAnalyser = new Analyser(pf);
-			
-			res=Math.random()*100;
-			System.out.println(res);
-		
-			if(firstAnalyser.getQuality()>beforeMove){
-				// Qu'est ce qu'il se passe
-			}
-			
-			
-			if(res<25)
-				str="u";
-			else if(res<50)
-				str="l";
-			else if(res<75)
-				str="d";
-			else
-				str="r";
-			
-			
-			
-			
-			
-			System.out.println(str);
-			int moved = pf.moveByString(str);
-	        if(moved>0) pf.insertRandCell();
-	        System.out.println("-------------------------");
-	        System.out.println(pf.maxValue().getValue());
-	        for (game.Tuple<Integer,Integer> resr : pf.maxValue().getPositions()) {
-	        	System.out.println(resr.x);
-	        	System.out.println(resr.y);
+		double pbU=25;
+		double pbL=pbU+25;
+		double pbD=pbL+25;
+		double pbR=pbD+25;
+		int won=-1;
+		for (int i = 0; i < 10; i++) {
 				
+			
+			PlayingField pf = new PlayingField(4,4);
+			pf.initialize(2);
+			System.out.println(pf);
+			previousMove="nn";
+			previousQuality=0;
+			while(pf.movesAvailable()){
+				firstAnalyser = new Analyser(pf);
+				
+				res=Math.random()*100;
+				System.out.println(res);
+			
+				// 
+				if(firstAnalyser.getQuality()>previousQuality){
+					switch (previousMove) {
+					case "u":
+						pbU+=0.75;
+						pbL-=0.25;
+						pbD-=0.25;
+						pbR-=0.25;
+						break;
+					case "l" :
+						pbU-=0.25;
+						pbL+=0.75;
+						pbD-=0.25;
+						pbR-=0.25;
+						break;
+					case "d":
+						pbU-=0.25;
+						pbL-=0.25;
+						pbD+=0.75;
+						pbR-=0.25;
+						break;
+					case "r" :
+						pbU-=0.25;
+						pbL-=0.25;
+						pbD+=0.75;
+						pbR-=0.25;
+						break;
+					default:
+						break;
+					}
+				}
+				else{
+					switch (previousMove) {
+					case "u":
+						pbU-=0.75;
+						pbL+=0.25;
+						pbD+=0.25;
+						pbR+=0.25;
+						break;
+					case "l" :
+						pbU+=0.25;
+						pbL-=0.75;
+						pbD+=0.25;
+						pbR+=0.25;
+						break;
+					case "d":
+						pbU+=0.25;
+						pbL+=0.25;
+						pbD-=0.75;
+						pbR+=0.25;
+						break;
+					case "r" :
+						pbU+=0.25;
+						pbL+=0.25;
+						pbD-=0.75;
+						pbR+=0.25;
+						break;
+					default:
+						break;
+					}
+				}
+				
+				
+				if(res<pbU)
+					str="u";
+				else if(res<pbL)
+					str="l";
+				else if(res<pbD)
+					str="d";
+				else
+					str="r";
+				
+				
+				System.out.println("-----------------PROBA");
+				System.out.println("U : " + pbU);
+				System.out.println("L : " + pbL);
+				System.out.println("D : " + pbD);
+				System.out.println("R : " + pbR);
+				
+				System.out.println(str);
+				int moved = pf.moveByString(str);
+		        if(moved>0) pf.insertRandCell();
+		        System.out.println("-------------------------");
+		        System.out.println(pf.maxValue().getValue());
+		        for (game.Tuple<Integer,Integer> resr : pf.maxValue().getPositions()) {
+		        	System.out.println(resr.x);
+		        	System.out.println(resr.y);
+					
+				}
+		        
+		        // Sauvegarde de l'état
+		        previousMove=str;
+		        previousQuality=firstAnalyser.getQuality();
+		        System.out.println("This is my quality " +firstAnalyser.getQuality());
+		        System.out.println(pf);
 			}
-	        beforeMove=firstAnalyser.getQuality();
-	        System.out.println(pf);
+			if(pf.isGameWon())
+				won=i;
+			
+			System.out.println(pf);
 		}
-		System.out.println(pf);
+		System.out.println(won);
 	}
 	
 }
