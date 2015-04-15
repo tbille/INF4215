4d3d578ff1ab40ab02a1a4404e3f6f2a3e2f809c
Jean-Marie Robin <JeanMarie@Sansfil-Securise-Etudiants-Lassonde-240-58.polymtl.ca>
2015-04-15 16:51:25 -0400

    proba foireuse

--- TP3/src/Game2048TP.java
+++ TP3/src/Game2048TP.java
@@ -14,52 +14,61 @@ public class Game2048TP {
 		Analyser firstAnalyser;
 		int previousQuality;
 		String previousMove;
-		
+
 		double pbU=25;
 		double pbL=pbU+25;
 		double pbD=pbL+25;
 		double pbR=pbD+25;
 		int won=-1;
 		for (int i = 0; i < 10; i++) {
-				
-			
+
+
 			PlayingField pf = new PlayingField(4,4);
 			pf.initialize(2);
 			System.out.println(pf);
 			previousMove="nn";
 			previousQuality=0;
+			double pb_tmp;
 			while(pf.movesAvailable()){
 				firstAnalyser = new Analyser(pf);
-				
+
 				res=Math.random()*100;
 				System.out.println(res);
-			
-				// 
+
+				//
 				if(firstAnalyser.getQuality()>previousQuality){
 					switch (previousMove) {
 					case "u":
-						pbU+=0.75;
-						pbL-=0.25;
-						pbD-=0.25;
-						pbR-=0.25;
+						pb_tmp=pbU;
+						pbU*=1.03;
+						pb_tmp=(pb_tmp-pbU)/3;
+						pbL-=pb_tmp;
+						pbD-=pb_tmp;
+						pbR-=pb_tmp;
 						break;
 					case "l" :
-						pbU-=0.25;
-						pbL+=0.75;
-						pbD-=0.25;
-						pbR-=0.25;
+						pb_tmp=pbL;
+						pbL*=1.03;
+						pb_tmp=(pb_tmp-pbL)/3;
+						pbU-=pb_tmp;
+						pbD-=pb_tmp;
+						pbR-=pb_tmp;
 						break;
 					case "d":
-						pbU-=0.25;
-						pbL-=0.25;
-						pbD+=0.75;
-						pbR-=0.25;
+						pb_tmp=pbD;
+						pbD*=1.03;
+						pb_tmp=(pb_tmp-pbD)/3;
+						pbU-=pb_tmp;
+						pbL-=pb_tmp;
+						pbR-=pb_tmp;
 						break;
 					case "r" :
-						pbU-=0.25;
-						pbL-=0.25;
-						pbD+=0.75;
-						pbR-=0.25;
+						pb_tmp=pbR;
+						pbR*=1.03;
+						pb_tmp=(pb_tmp-pbR)/3;
+						pbU-=pb_tmp;
+						pbL-=pb_tmp;
+						pbD-=pb_tmp;
 						break;
 					default:
 						break;
@@ -67,36 +76,44 @@ public class Game2048TP {
 				}
 				else{
 					switch (previousMove) {
-					case "u":
-						pbU-=0.75;
-						pbL+=0.25;
-						pbD+=0.25;
-						pbR+=0.25;
-						break;
-					case "l" :
-						pbU+=0.25;
-						pbL-=0.75;
-						pbD+=0.25;
-						pbR+=0.25;
-						break;
-					case "d":
-						pbU+=0.25;
-						pbL+=0.25;
-						pbD-=0.75;
-						pbR+=0.25;
-						break;
-					case "r" :
-						pbU+=0.25;
-						pbL+=0.25;
-						pbD-=0.75;
-						pbR+=0.25;
-						break;
-					default:
-						break;
+						case "u":
+							pb_tmp=pbU;
+							pbU*=0.97;
+							pb_tmp=(pb_tmp-pbU)/3;
+							pbL-=pb_tmp;
+							pbD-=pb_tmp;
+							pbR-=pb_tmp;
+							break;
+						case "l" :
+							pb_tmp=pbL;
+							pbL*=0.97;
+							pb_tmp=(pb_tmp-pbL)/3;
+							pbU-=pb_tmp;
+							pbD-=pb_tmp;
+							pbR-=pb_tmp;
+							break;
+						case "d":
+							pb_tmp=pbD;
+							pbD*=0.97;
+							pb_tmp=(pb_tmp-pbD)/3;
+							pbU-=pb_tmp;
+							pbL-=pb_tmp;
+							pbR-=pb_tmp;
+							break;
+						case "r" :
+							pb_tmp=pbR;
+							pbR*=0.97;
+							pb_tmp=(pb_tmp-pbR)/3;
+							pbU-=pb_tmp;
+							pbL-=pb_tmp;
+							pbD-=pb_tmp;
+							break;
+						default:
+							break;
 					}
 				}
-				
-				
+
+
 				if(res<pbU)
 					str="u";
 				else if(res<pbL)
@@ -105,14 +122,14 @@ public class Game2048TP {
 					str="d";
 				else
 					str="r";
-				
-				
+
+
 				System.out.println("-----------------PROBA");
 				System.out.println("U : " + pbU);
 				System.out.println("L : " + pbL);
 				System.out.println("D : " + pbD);
 				System.out.println("R : " + pbR);
-				
+
 				System.out.println(str);
 				int moved = pf.moveByString(str);
 		        if(moved>0) pf.insertRandCell();
@@ -121,9 +138,9 @@ public class Game2048TP {
 		        for (game.Tuple<Integer,Integer> resr : pf.maxValue().getPositions()) {
 		        	System.out.println(resr.x);
 		        	System.out.println(resr.y);
-					
+
 				}
-		        
+
 		        // Sauvegarde de l'état
 		        previousMove=str;
 		        previousQuality=firstAnalyser.getQuality();
@@ -132,10 +149,10 @@ public class Game2048TP {
 			}
 			if(pf.isGameWon())
 				won=i;
-			
+
 			System.out.println(pf);
 		}
 		System.out.println(won);
 	}
-	
+
 }
