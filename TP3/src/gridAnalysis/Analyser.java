239af7b2b29c8bed671e8836f763087ac4106e02
Jean-Marie Robin <JeanMarie@Sansfil-Securise-Etudiants-Lassonde-240-58.polymtl.ca>
2015-04-15 15:37:06 -0400

    ignore analyser

--- TP3/src/gridAnalysis/Analyser.java
+++ TP3/src/gridAnalysis/Analyser.java
@@ -1,5 +1,33 @@
 package gridAnalysis;
 
-public class Analyser {
+import game.PlayingField;
 
+public class Analyser {
+	
+	private PlayingField pf;
+	private int Quality;
+	
+	private int maxValue;
+	
+	public Analyser(PlayingField _pf) {
+		pf = _pf;
+		maxValue = pf.maxValue();
+	}
+	
+	private void checkPosition(){
+		//Max Value in the Bottom-Left cell
+	}
+	
+	private void checkLastRow(){
+		//Consecutive Values in the last row
+	} 
+	
+	private void checkToBeUsed(){
+		//Two adjacent cells with the same values
+	}
+	
+	private void checkIsolated(){
+		//Value that can not be used easily
+	}
+	
 }
